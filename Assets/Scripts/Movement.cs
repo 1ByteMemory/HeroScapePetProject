ce2ba9968ae5fc094e234588c285a34afe561155
Dan J <dan.jones8902@gmail.com>
2020-07-27 10:45:44 +0100

    Tiles change material when highlighted
    Getting a script to highlight available move spaces

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    public int moveSpaces = 2;
+    public Material availableSpacesMat;
+    public Material landingSpaceMat;
+
+    GameObject[] availableSpaces;
+
+    int spacesUsed = 0;
+    RaycastHit rayHit;
+
+    // The size of the hexs
+    const float hexSize = 1.72f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,48 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+            ShowAvailableSpaces(true);
+		}
+        if (Input.GetKeyUp(KeyCode.Space))
+		{
+            ShowAvailableSpaces(false);
+		}
+
+
     }
+
+    void ShowAvailableSpaces(bool isHighlighting)
+	{
+        Ray ray = new Ray(transform.position + new Vector3(0, -0.3f, 0), transform.forward);
+
+        // Raycast to get all space the character can move to
+        while (Physics.Raycast(ray, out rayHit, hexSize) && spacesUsed < moveSpaces)
+		{
+            Debug.Log(spacesUsed);
+            spacesUsed++;
+
+            Vector3 newPos = rayHit.transform.position + new Vector3(0, -0.05f);
+
+            // Change Ray to be sent out from rayHit tile
+            ray = new Ray(newPos, transform.forward);
+
+            if (isHighlighting)
+                rayHit.transform.GetComponent<Tile>().ChangeMaterial(availableSpacesMat);
+            else
+                rayHit.transform.GetComponent<Tile>().ChangeToDefault();
+
+		}
+
+        spacesUsed = 0;
+	}
+
+
+	void Move(int moveSpaces)
+	{
+
+	}
+
+
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    Material defaultMat;
+
+    [HideInInspector]
+    public bool isHighlighted;
+    [HideInInspector]
+    public Material newMat;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultMat = GetComponentsInChildren<Renderer>()[1].material;
+        newMat = defaultMat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ChangeToDefault();
+    }
+
+    public void ChangeToDefault()
+	{
+        GetComponentsInChildren<Renderer>()[1].material = defaultMat;
+        /*
+        if (newMat != defaultMat)
+        {
+            //isHighlighted = false;
+            Debug.Log("Changing back to default");
+            newMat = defaultMat;
+        }*/
+    }
+
+    public void ChangeMaterial(Material material)
+	{
+        GetComponentsInChildren<Renderer>()[1].material = material;
+        
+        //isHighlighted = true;
+        //newMat = material;
+
+    }
+
+}
