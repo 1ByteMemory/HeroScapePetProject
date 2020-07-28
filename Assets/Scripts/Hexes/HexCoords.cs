2ad11010b468135320a210a8bc0232001b8d899f
Dan J <dan.jones8902@gmail.com>
2020-07-28 17:01:46 +0100

    Following a tutorial on generating a hex map

---
+++ Assets/Scripts/Hexes/HexCoords.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct HexCoords
+{
+	public int X { get; private set; }
+	public int Z { get; private set; }
+
+	public HexCoords(int x, int z)
+	{
+		X = x;
+		Z = z;
+	}
+
+	public static HexCoords FromOffsetCoords (int x, int z)
+	{
+		return new HexCoords(x - z / 2, z);
+	}
+
+
+	public override string ToString()
+	{
+		return "(" + X.ToString() + ", " + Z.ToString() + ")";
+	}
+
+	public string ToStringOnSeperateLines()
+	{
+		return X.ToString() + "\n" + Z.ToString();
+	}
+
+}
---
+++ Assets/Scripts/Hexes/HexGrid.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrfab;
+
+	public Text cellLabelPrefab;
+	Canvas gridCanvas;
+
+	HexMesh hexMesh;
+
+    HexCell[] cells;
+
+	private void Awake()
+	{
+		gridCanvas = GetComponentInChildren<Canvas>();
+		hexMesh = GetComponentInChildren<HexMesh>();
+
+		cells = new HexCell[height * width];
+
+		for (int z = 0, i = 0; z < height; z++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+
+	private void Start()
+	{
+		hexMesh.Triangulate(cells);
+	}
+
+	void CreateCell(int x, int z, int i)
+	{
+		Vector3 position;
+		position.x = (x + z * 0.5f - z / 2) * (HexMatrics.innerRadius * 2);
+		position.y = 0;
+		position.z = z * (HexMatrics.outerRadius * 1.5f);
+
+		// Create hex cells
+		HexCell cell = cells[i] = Instantiate(cellPrfab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+		cell.coords = HexCoords.FromOffsetCoords(x, z);
+
+
+		// Make hex cells more visable
+		Text label = Instantiate(cellLabelPrefab);
+		label.rectTransform.SetParent(gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+		label.text = cell.coords.ToStringOnSeperateLines();
+
+
+	}
+
+}
